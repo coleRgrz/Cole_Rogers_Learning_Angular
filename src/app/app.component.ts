63986c9945152c843faf110dd4fc2ef441488fc8
coleRgrz <w0827970@myscc.ca>
2024-09-13 17:20:12 -0400

    Finished steps 5 and 6, completed assignment 2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Person} from "./Shared/Modules/user";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = "People Generation";
-
-  person1 : Person = {name: "Cole", age: 20, faveFood: "Brisket", hairColour: "Black", isTeamLead: undefined};
-  person2 : Person = {name: "Colton", age: 19, faveFood: "Pizza", hairColour: "Brown", isTeamLead: undefined};
+  title = "List of People";
+  // All the people I have created
+  person1 : Person = {name: "Cole", age: 20, faveFood: "Brisket", hairColour: "Black", isTeamLead: false};
+  person2 : Person = {name: "Colton", age: 19, faveFood: "Pizza", hairColour: "Brown", isTeamLead: false};
   person3 : Person = {name: "Andrew", age: 20, faveFood: "Whopper", hairColour: "Brown", isTeamLead: true};
+  person4 : Person = {name: "Madison", age: 23, faveFood: "Sandwich", hairColour: "Black", isTeamLead: true};
+  person5 : Person = {name: "Sarah", age: 23, faveFood: "Chicken", hairColour: "Purple", isTeamLead: false};
+  person6 : Person = {name: "Noah", age: 20, faveFood: "Taco", hairColour: "Black", isTeamLead: false};
+  //An array to store all the people
+  personArray: Person[] = [this.person1, this.person2, this.person3, this.person4, this.person5, this.person6];
 
-
-
+  //toggle lead status for my optional property
+  toggleLeadStatus(person: Person): void {
+    person.isTeamLead = !person.isTeamLead;
+  }
 }
 
 
