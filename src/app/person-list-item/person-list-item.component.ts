38ba56548ce904edc9e0ed232a2ace58ea03d577
Cole Rogers <w0827970@myscc.ca>
2024-09-25 18:05:25 -0400

    completed step 4. Created Person-list and person-list-Item

---
+++ src/app/person-list-item/person-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './person-list-item.component.html',
+  styleUrl: './person-list-item.component.css'
+})
+export class PersonListItemComponent {
+
+}
