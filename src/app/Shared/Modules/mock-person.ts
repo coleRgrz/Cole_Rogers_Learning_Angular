d030ff82ccfa852c07a36c1749391ae45d58d9d8
Cole Rogers <w0827970@myscc.ca>
2024-10-02 18:01:21 -0400

    Created mock content and generated a service. Took array from app.component.ts and dumped it into mock-person.ts

---
+++ src/app/Shared/Modules/mock-person.ts
@@ -0,0 +1,11 @@
+import {Person} from "./user";
+
+export const PersonList: Person[] = [
+  // All the people I have created
+  {id: 1, name: "Cole", age: 20, faveFood: "Brisket", hairColour: "Black", isTeamLead: false},
+  {id: 2, name: "Colton", age: 19, faveFood: "Pizza", hairColour: "Brown", isTeamLead: false},
+  {id: 3, name: "Andrew", age: 20, faveFood: "Whopper", hairColour: "Brown", isTeamLead: true},
+  {id: 4, name: "Madison", age: 23, faveFood: "Sandwich", hairColour: "Black", isTeamLead: true},
+  {id: 5, name: "Sarah", age: 23, faveFood: "Chicken", hairColour: "Purple", isTeamLead: false},
+  {id: 6, name: "Noah", age: 20, faveFood: "Taco", hairColour: "Black", isTeamLead: false},
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,7 @@ import {PersonListComponent} from "./person-list/person-list.component";
 })
 export class AppComponent {
   title = "List of People";
-  // All the people I have created
-  person1 : Person = {name: "Cole", age: 20, faveFood: "Brisket", hairColour: "Black", isTeamLead: false};
-  person2 : Person = {name: "Colton", age: 19, faveFood: "Pizza", hairColour: "Brown", isTeamLead: false};
-  person3 : Person = {name: "Andrew", age: 20, faveFood: "Whopper", hairColour: "Brown", isTeamLead: true};
-  person4 : Person = {name: "Madison", age: 23, faveFood: "Sandwich", hairColour: "Black", isTeamLead: true};
-  person5 : Person = {name: "Sarah", age: 23, faveFood: "Chicken", hairColour: "Purple", isTeamLead: false};
-  person6 : Person = {name: "Noah", age: 20, faveFood: "Taco", hairColour: "Black", isTeamLead: false};
-  //An array to store all the people
-  personArray: Person[] = [this.person1, this.person2, this.person3, this.person4, this.person5, this.person6];
+
 
   //toggle lead status for my optional property
   toggleLeadStatus(person: Person): void {
