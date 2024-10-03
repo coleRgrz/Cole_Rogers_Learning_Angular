59aadbb21b8f98dd40096f958944120f71bdcb93
Cole Rogers <w0827970@myscc.ca>
2024-10-02 20:38:42 -0400

    completed steps 10-12. Added PersonListItem tag to appComponent, added service to appComponent, used the service to retrieve person item (id 4) and populated the PersonListItem tag with the same person item.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Person} from "./Shared/Modules/user";
 import {NgForOf, NgIf} from "@angular/common";
 import {PersonListComponent} from "./person-list/person-list.component";
+import {PersonService} from "./services/person.service";
+import {PersonListItemComponent} from "./person-list-item/person-list-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf, PersonListComponent],
+  imports: [RouterOutlet, NgForOf, NgIf, PersonListComponent, PersonListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = "List of People";
+export class AppComponent implements OnInit{
+  constructor(private personService: PersonService) {
+  }
+
+  selectedPerson: Person | undefined;
+
+  ngOnInit() {
+    this.getSelectedPerson(4);
+  }
+
+  getSelectedPerson(id: number): void{
+    this.personService.getPersonById(id).subscribe({
+      next: (person) => this.selectedPerson = person,
+      error: err => console.error("Error fetching Person by id", err)
+    });
+  }
+
+  //title = "List of People";
 }
 
 
