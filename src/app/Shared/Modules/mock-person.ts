099946419ea649ef1a3512b851a22fdc26f7306e
Cole Rogers <w0827970@myscc.ca>
2024-10-09 01:27:22 -0400

    changed images to better represent the properties of each person

--- src/app/Shared/Modules/mock-person.ts
+++ src/app/Shared/Modules/mock-person.ts
@@ -2,10 +2,10 @@ import {Person} from "./user";
 
 export const PersonList: Person[] = [
   // All the people I have created
-  {id: 1, name: "Cole", age: 20, faveFood: "Brisket", hairColour: "Black", isTeamLead: false, img:'https://as1.ftcdn.net/v2/jpg/00/26/35/66/1000_F_26356614_lUktFPqBJSupno7xp5fuDIkMEi1d1Nud.jpg'},
-  {id: 2, name: "Colton", age: 19, faveFood: "Pizza", hairColour: "Brown", isTeamLead: false, img:'https://thumbs.dreamstime.com/z/happy-man-white-teeth-smiling-thumbs-up-isolated-background-40783976.jpg'},
-  {id: 3, name: "Andrew", age: 20, faveFood: "Whopper", hairColour: "Brown", isTeamLead: true, img: 'https://clipart-library.com/images_k/man-transparent-background/man-transparent-background-3.png'},
-  {id: 4, name: "Madison", age: 23, faveFood: "Sandwich", hairColour: "Black", isTeamLead: true, img: 'https://as1.ftcdn.net/v2/jpg/01/63/64/76/1000_F_163647690_DsAQGsZRdUD3sTrslaZ1gsR44bnTDi4G.jpg'},
-  {id: 5, name: "Sarah", age: 23, faveFood: "Chicken", hairColour: "Purple", isTeamLead: false, img: 'https://t3.ftcdn.net/jpg/01/85/04/84/360_F_185048418_X1kohHSgyAbPJQxPHurs4uXCTmcRSNAp.jpg'},
-  {id: 6, name: "Noah", age: 20, faveFood: "Taco", hairColour: "Black", isTeamLead: false, img: 'https://media.gettyimages.com/id/183279907/photo/happy-young-man-gives-thumbs-up.jpg?s=612x612&w=gi&k=20&c=7cVomQPYQ-7oPKsfSjKF4gth3v6KDZA34pfKw-aRfK0='},
+  {id: 1, name: "Cole", age: 20, faveFood: "Brisket", hairColour: "Black", isTeamLead: false, img: "https://media.istockphoto.com/id/483627817/photo/showing-off-his-pearly-whites.jpg?s=612x612&w=0&k=20&c=gk6aVVGp52YFx1ZzPVQplGc7JL5zkrfxQTuLjIn2RU8="},
+  {id: 2, name: "Colton", age: 19, faveFood: "Pizza", hairColour: "Brown", isTeamLead: false, img:"https://thumbs.dreamstime.com/z/happy-man-white-teeth-smiling-thumbs-up-isolated-background-40783976.jpg"},
+  {id: 3, name: "Andrew", age: 20, faveFood: "Whopper", hairColour: "Brown", isTeamLead: true, img: "https://clipart-library.com/images_k/man-transparent-background/man-transparent-background-3.png"},
+  {id: 4, name: "Madison", age: 23, faveFood: "Sandwich", hairColour: "Black", isTeamLead: true, img: "https://www.shutterstock.com/image-photo/beautiful-young-woman-long-black-600nw-1014456400.jpg"},
+  {id: 5, name: "Sarah", age: 23, faveFood: "Chicken", hairColour: "Purple", isTeamLead: false, img: "https://media.istockphoto.com/id/1218693050/photo/swinging-hair-2.jpg?s=612x612&w=0&k=20&c=awsWoyWwq9J9avzwcUS7qKCobokoVqmxdc6E0oCLYkQ="},
+  {id: 6, name: "Noah", age: 20, faveFood: "Taco", hairColour: "Black", isTeamLead: false, img: "https://media.gettyimages.com/id/183279907/photo/happy-young-man-gives-thumbs-up.jpg?s=612x612&w=gi&k=20&c=7cVomQPYQ-7oPKsfSjKF4gth3v6KDZA34pfKw-aRfK0="}
 ]
