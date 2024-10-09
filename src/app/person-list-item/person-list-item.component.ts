df5f3d51bc2ea874cefe2eab7f22fb29e50a2116
Cole Rogers <w0827970@myscc.ca>
2024-10-09 00:50:43 -0400

    fixed images

--- src/app/person-list-item/person-list-item.component.ts
+++ src/app/person-list-item/person-list-item.component.ts
@@ -1,11 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {Person} from "../Shared/Modules/user";
 import {PersonList} from "../Shared/Modules/mock-person";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-person-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './person-list-item.component.html',
   styleUrl: './person-list-item.component.css'
 })
