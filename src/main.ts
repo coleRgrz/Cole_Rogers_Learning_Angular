d5c92913cdfcc478e86ac71a989b266abf7f8ed3
Cole Rogers <w0827970@myscc.ca>
2024-10-08 22:55:04 -0400

    Define list of routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {PersonListComponent} from "./app/person-list/person-list.component";
+import {PersonListItemComponent} from "./app/person-list-item/person-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path: '', redirectTo: '/Person', pathMatch: 'full'},
+  {path: 'Person', component: PersonListComponent},
+  {path: 'Person/:id', component: PersonListItemComponent}
+]
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
